8a27303dbee4d1ca9b71ba5caa5c24739a6eb314
Amit Kumar Khare <amitkhare@users.noreply.github.com>
2016-09-10 22:08:03 +0530

    Update T.php

--- T.php
+++ T.php
@@ -1,1 +1,14 @@
-<?php class T{public $p;function v($f,$d=[]) {foreach ($d as $k=>$v) {$$k=$v;}ob_start();require($this->p.$f.'.php');return ob_get_clean();}}
+<?php
+// URL : https://github.com/amitkhare/SmallestTemplateEngine
+// ### USAGE ###
+// $theme = new T();
+// $theme->p = __DIR__."/../resources/";
+// $data = [
+//     'var1'=>'value1',
+//     'var2'=>'value2'
+// ];
+// echo $theme->v('templates/header',$data);
+// echo $theme->v('views/pages/home',$data);
+// echo $theme->v('templates/footer',$data);
+
+class T{public $p;function v($f,$d=[]) {foreach ($d as $k=>$v) {$$k=$v;}ob_start();require($this->p.$f.'.php');return ob_get_clean();}}
