7731f13106babeec8c9d74c94d5daddf6a2c848e
Amit Kumar Khare <amitkhare@users.noreply.github.com>
2016-09-10 21:53:49 +0530

    Update T.php

--- T.php
+++ T.php
@@ -1,1 +1,1 @@
-class T{public $p;function v($f,$d=[]) {foreach ($d as $k=>$v) {$$k=$v;}ob_start();require($this->p.$f.'.php');return ob_get_clean();}}
+<php class T{public $p;function v($f,$d=[]) {foreach ($d as $k=>$v) {$$k=$v;}ob_start();require($this->p.$f.'.php');return ob_get_clean();}}
