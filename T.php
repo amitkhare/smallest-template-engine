3ec9d88e66aa8cb291b810ad12e8f07cd9fabd28
Amit Kumar Khare <amitkhare@users.noreply.github.com>
2016-09-10 23:53:54 +0530

    Update T.php

--- T.php
+++ T.php
@@ -11,4 +11,4 @@
 // echo $theme->v('views/pages/home',$data);
 // echo $theme->v('templates/footer',$data);
 
-class T{public $p;function v($f,$d=[]) {foreach ($d as $k=>$v) {$$k=$v;}ob_start();require($this->p.$f.'.php');return ob_get_clean();}}
+class T{public $p;function v($f,$d=[]){foreach($d as $k=>$v){$$k=$v;}ob_start();require($this->p.$f.'.php');return ob_get_clean();}}
